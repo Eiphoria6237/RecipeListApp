61c63fa97f9621d9c95cbdc703b07cb08204f108
Eiphoria <liuerbsb@gmal.com>
2021-03-15 15:47:17 +0900

    Initial Commit

---
+++ RecipeList_1/RecipeList_1App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeList_1App.swift
+//  RecipeList_1
+//
+//  Created by LIU SHURUI on 2021/03/15.
+//
+
+import SwiftUI
+
+@main
+struct RecipeList_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
