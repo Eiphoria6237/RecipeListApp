29a7b8c6ed75a1c6f2d83334f039ad8843943603
Eiphoria <liuerbsb@gmal.com>
2021-03-16 15:47:10 +0900

    changed the list view to scrollView, LazyVStack,  added environmentObject for preview

--- RecipeList_1/Views/RecipeListView.swift
+++ RecipeList_1/Views/RecipeListView.swift
@@ -15,19 +15,32 @@ struct RecipeListView: View {
         
         
         NavigationView {
-            List(model.recipes) { r in
-                
-                NavigationLink (destination:RecipeDetailView(recipe:r), label:{
+            VStack(alignment:.leading) {
+                Text("All Recipes")
+                    .bold()
                     
-                
-                HStack(spacing:20) {
-                    Image(r.image).resizable().scaledToFill().frame(width:50,height:50,alignment:.center).clipped()
-                    Text(r.name)
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                ScrollView {
+                    
+                    LazyVStack(alignment:.leading) {
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink (destination:RecipeDetailView(recipe:r), label:{
+                                
+                                
+                                HStack(spacing:20) {
+                                    Image(r.image).resizable().scaledToFill().frame(width:50,height:50,alignment:.center).clipped()
+                                    Text(r.name)
+                                        .foregroundColor(.black)
+                                }
+                            })
+                        }
+                    }
                 }
-            })
-        }
-            
-        .navigationBarTitle("All Recipes")
+            }
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
@@ -35,5 +48,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
