3c6d424669778de954465909c7e375730141caa1
Eiphoria <liuerbsb@gmal.com>
2021-03-16 14:11:33 +0900

    featured view

--- RecipeList_1/Views/RecipeFeaturedView.swift
+++ RecipeList_1/Views/RecipeFeaturedView.swift
@@ -12,12 +12,63 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment:.leading, spacing:0) {
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
+                TabView {
+                    ForEach (0..<model.recipes.count) { index in
+                        
+                        if model.recipes[index].featured == true {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                
+                                VStack(spacing:0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(.vertical,5)
+                                }
+                            }
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                            .cornerRadius(15)
+                            .shadow(color:Color(hue: 0, saturation: 0, brightness: 0, opacity: 0.5), radius: 10, x:-5,y:5)
+                                
+                        }
+                        
+                    }
+                    
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            
+            VStack(alignment:.leading, spacing:10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }
+            .padding(.leading)
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
